50a5208e2e24c3509acd74ab8575687d58ea3bde
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-01-14 00:48:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-// Layout Components
-import Navbar from './componets/Navbar'
-import Footer from './componets/Footer'
-import Intro from './componets/Intro'
-import Home from './componets/Home'
-import MainServices from './componets/MainServices'
-import Projectpage from './componets/Projectpage'
-import Contactpage from './componets/Contactpage'
-import MainAbout from './componets/MainAbout'
-import Thankspage from './componets/Thankspage'
+// Layout
+import Navbar from "./componets/Navbar";
+import Footer from "./componets/Footer";
+import Intro from "./componets/Intro";
+
+// Pages
+import Home from "./componets/Home";
+import MainServices from "./componets/MainServices";
+import Projectpage from "./componets/Projectpage";
+import Contactpage from "./componets/Contactpage";
+import MainAbout from "./componets/MainAbout";
+import Thankspage from "./componets/Thankspage";
 
 // Scroll to top on route change
 const ScrollToTop = () => {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return null
-}
+  return null;
+};
 
 const App = () => {
-  // 1. Initialize showIntro to true so it shows immediately on reload
-  const [showIntro, setShowIntro] = useState(true)
+  const [showIntro, setShowIntro] = useState(true);
 
   useEffect(() => {
-    // 2. Simply set a timer to hide the intro after your desired duration (4s)
     const timer = setTimeout(() => {
-      setShowIntro(false)
-    }, 4000)
+      setShowIntro(false);
+    }, 4000); // intro duration
 
-    // Cleanup timer if component unmounts
-    return () => clearTimeout(timer)
-  }, []) // Empty dependency array ensures this runs once per hard reload
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
       <ScrollToTop />
 
-      {/* 3. Render Intro if showIntro is true */}
-      {showIntro ? (
-        <Intro />
-      ) : (
-        <>
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/mainabout" element={<MainAbout />} />
-              <Route path="/mainservices" element={<MainServices />} />
-              <Route path="/projectpage" element={<Projectpage />} />
-              <Route path="/contactpage" element={<Contactpage />} />
-              <Route path='/thankspage' element={<Thankspage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </>
-      )}
-    </>
-  )
-}
+      {/* MAIN APP (ALWAYS MOUNTED) */}
+      <div className="min-h-screen bg-[#050505] text-white">
+        <Navbar />
 
-export default App
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mainabout" element={<MainAbout />} />
+            <Route path="/mainservices" element={<MainServices />} />
+            <Route path="/projectpage" element={<Projectpage />} />
+            <Route path="/contactpage" element={<Contactpage />} />
+            <Route path="/thankspage" element={<Thankspage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+
+      {/* INTRO OVERLAY */}
+      {showIntro && <Intro />}
+    </>
+  );
+};
+
+export default App;
