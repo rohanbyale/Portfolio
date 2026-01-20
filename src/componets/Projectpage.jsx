8b24b46ac6091246ceed6d204ae5e46b42d7d258
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-01-20 16:40:04 +0530

    Update Projectpage.jsx

--- src/componets/Projectpage.jsx
+++ src/componets/Projectpage.jsx
@@ -3,17 +3,20 @@ import Project from './Project'
 import Projectone from './Projectone'
 import ProcessSection from './ProcessSection'
 import Projectmarque from './Projectmarque'
-
+import PageTransition from './PageTransition'
 const Projectpage = () => {
   return (
     <div>
+      <PageTransition>
         
         <Projectone  />
         <Projectmarque />
       <Project />
       <ProcessSection />
+        </PageTransition>
     </div>
   )
 }
+
 
 export default Projectpage
