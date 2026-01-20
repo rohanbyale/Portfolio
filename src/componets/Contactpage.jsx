632a92c0a91a2f7855ad865c88ca3232a01ed2f1
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-01-20 16:42:27 +0530

    Update Contactpage.jsx

--- src/componets/Contactpage.jsx
+++ src/componets/Contactpage.jsx
@@ -4,17 +4,20 @@ import ContactHero from './ContactHero'
 import ContactForm from './ContactForm'
 import Question from './Question'
 import Contactmarque from './Contactmarque'
-
+import PageTransition from './PageTransition'
 const Contactpage = () => {
   return (
     <div>
+      <PageTransition>
        <ContactHero />
        <ContactForm />
       
        <Question />
         <Contactmarque />
+        </PageTransition>
     </div>
   )
 }
+
 
 export default Contactpage
