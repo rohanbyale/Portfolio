d6cdffcc57717ed0c9b3ec56adf55d86035afdda
Rohan Byale <132317423+rohanbyale@users.noreply.github.com>
2026-05-25 16:47:01 +0530

    Update Herosection.jsx

--- src/componets/Herosection.jsx
+++ src/componets/Herosection.jsx
@@ -386,7 +386,7 @@ const HeroSection = () => {
             </motion.div>
 
             <motion.a
-              href="/Resume.pdf"
+              href="/Resumee.pdf"
               download
               whileHover={{ x: 5, color: "#facc15" }}
               className="flex items-center gap-3 text-neutral-400 text-[11px] font-black uppercase tracking-widest transition-all cursor-pointer"
